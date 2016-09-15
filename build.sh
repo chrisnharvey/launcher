9f3632b2af3e0fdbed921dd9093ee110896c41e0
Chris Harvey <chris@chrisnharvey.com>
2016-09-15 18:52:43 +0100

    Adding build script and AppImage.yml

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir -p /mnt/appimager/build
+
+gcc loopmounter.c -o /mnt/appimager/build/usr/bin/loopmounter
+strip /mnt/appimager/build/usr/bin/loopmounter
+
+cp -R usr /mnt/appimager/build
+
+chown root:root /mnt/appimager/build/usr/bin/loopmounter
+chmod 4755 /mnt/appimager/build/usr/bin/loopmounter
+
+rm -f 0.9.42.tar.gz
+wget https://github.com/netblue30/firejail/archive/0.9.42.tar.gz
+tar xf 0.9.42.tar.gz
+cd firejail-0.9.42
+
+./configure --prefix=/mnt/appimager/build/usr
+make
+make install
+
+cd ..
+
+rm -rf firejail-0.9.42 0.9.42.tar.gz
