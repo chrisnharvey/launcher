5ddc6d363021484987f6ed9250184ffde0835874
Chris Harvey <chris@chrisnharvey.com>
2016-09-16 17:12:59 +0100

    Replace loopmounter with fuseiso

--- build.sh
+++ build.sh
@@ -3,12 +3,6 @@
 mkdir -p /mnt/appimager/build
 cp -R /mnt/appimager/usr /mnt/appimager/build
 
-gcc loopmounter.c -o /mnt/appimager/build/usr/bin/loopmounter
-strip /mnt/appimager/build/usr/bin/loopmounter
-
-chown root:root /mnt/appimager/build/usr/bin/loopmounter
-chmod 4755 /mnt/appimager/build/usr/bin/loopmounter
-
 rm -f 0.9.42.tar.gz
 wget https://github.com/netblue30/firejail/archive/0.9.42.tar.gz
 tar xf 0.9.42.tar.gz
